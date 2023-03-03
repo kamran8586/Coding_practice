038db27fa5f8d754a681af2a124f1bf106a037a3
Kamran <muhammadkamran8586@gmail.com>
2023-03-03 15:09:24 +0500

    Create Product_Max_Min_element_array.cpp

---
+++ Product_Max_Min_element_array.cpp
@@ -0,0 +1,20 @@
+/*
+Given two arrays of A and B respectively of sizes N1 and N2, the task is to calculate the product of the maximum element of the first array and minimum element of the second array.
+*/
+long long find_multiplication(int a[], int b[], int n, int m)
+    {
+        // Complete the function
+        int max = INT_MIN;
+        int min = INT_MAX;
+        for(int i = 0; i< n; i++){
+            if(max < a[i]){
+                max = a[i];
+            }
+        }
+        for(int i = 0; i< m; i++){
+            if(min > b[i]){
+                min = b[i];
+            }
+        }
+        return max*min;
+    }
